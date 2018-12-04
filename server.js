454d496324fb9476329ca855130b9acad2628ca0
Garry Maghari <garry.maghari@gmail.com>
2018-12-03 18:15:21 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3560;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+// Required Routes
+// ============================================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
