b88fa88d8f45c9d9acbb94fdf3a93674d957b040
Garry Maghari <garry.maghari@gmail.com>
2018-12-06 20:52:06 -0800

    Fix best match results in survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,4 +16,40 @@ module.exports = function (app) {
   app.get("./api/friends", function (req, res) {
     res.json(friendData);
   });
+
+  app.post('./api/friends', function(req, res) {
+    var thisUser = req.body;
+    var differences = [];
+
+    if (friendData.length > 1) {
+        friendData.forEach(function(user) {
+            var totalDifference = 0;
+
+            for (var i = 0; i < thisUser.answers.length; i++) {
+                var otherAnswer = user.answers[i];
+                var thisAnswer = thisUser.answers[i];
+                var difference = +otherAnswer - +thisAnswer;
+                totalDifference += Math.abs(difference);
+            }
+
+            differences.push(totalDifference);
+        });
+
+        var minimumDifference = Math.min.apply(null, differences);
+
+        var bestMatch = [];
+
+        for (var i = 0; i < differences.length; i++) {
+            if (differences[i] === minimumDifference) {
+                bestMatch.push(friendData[i]);
+            }
+        }
+
+        res.json(bestMatch);
+    } else {
+        res.json(friendData);
+    }
+
+    friendData.push(thisUser);
+  });
 }
