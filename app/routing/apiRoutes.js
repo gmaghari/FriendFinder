b319be8e791d17ed6b57ec0dd1961be69600e4ba
Garry Maghari <garry.maghari@gmail.com>
2018-12-06 20:59:24 -0800

    Test2

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,37 +13,50 @@ module.exports = function (app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
     // ---------------------------------------------------------------------------
-  app.get("./api/friends", function (req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendData);
   })
 
-  // Create New Characters - takes in JSON input
-app.post("./api/friends", function(req, res) {
-	var newFriend = req.body;
-	var newScore = 0;
-	var total = 0;
-	var match = {
-		name: "",
-		photo: "",
-		difference: 10000
-	}
+	app.post('/api/friends', function(req, res){
+		var newFriend = req.body;
 
-	// Calculating totals 
-	for (var i = 0; i < friendsList.length; i++) {
-		total = 0;
-
-		for (var j = 0; j < friendsList[i].preferences.length; j++) {
-			total += Math.abs(friendsList[i].preferences[j] - newFriend.preferences[j]);
-
-			if (total <= match.difference) {
-				match.name = friendsList[i].name,
-				match.photo = friendsList[i].photo,
-				match.difference = total
+		for(var i = 0; i < newFriend.scores.length; i++) {
+			if(newFriend.scores[i] == "1 (Strongly Disagree)") {
+				newFriend.scores[i] = 1;
+			} else if(newFriend.scores[i] == "5 (Strongly Agree)") {
+				newFriend.scores[i] = 5;
+			} else {
+				newFriend.scores[i] = parseInt(newFriend.scores[i]);
 			}
-    	}
-    }
-    friendsList.push(newFriend);
-    res.json(match);
-    console.log(match);
-});
+		}
+
+		var differencesArray = [];
+
+		for(var i = 0; i < friendData.length; i++) {
+
+			var comparedFriend = friendData[i];
+			var totalDifference = 0;
+			
+			for(var k = 0; k < comparedFriend.scores.length; k++) {
+				var differenceOneScore = Math.abs(comparedFriend.scores[k] - newFriend.scores[k]);
+				totalDifference += differenceOneScore;
+			}
+
+			differencesArray[i] = totalDifference;
+		}
+
+		var bestFriendNum = differencesArray[0];
+		var bestFriendIndex = 0;
+
+		for(var i = 1; i < differencesArray.length; i++) {
+			if(differencesArray[i] < bestFriendNum) {
+				bestFriendNum = differencesArray[i];
+				bestFriendIndex = i;
+			}
+		}
+
+		friendData.push(newFriend);
+
+		res.json(friendData[bestFriendIndex]);
+	})
 }
